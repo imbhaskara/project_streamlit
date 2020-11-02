74e7daf93a730b9c7713c647b0b30b22af8f018a
rickynauvaldy <ricky.nauvaldy@gmail.com>
2020-11-03 00:00:34 +0700

    Append photo to profile
    Append images manually to each html part

--- app.py
+++ app.py
@@ -7,63 +7,82 @@ import seaborn as sns
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
+import base64
 
 #import data
 drive_path = './'
 df = pd.read_excel(drive_path+'Komen Play Among Us1.xlsx',index_col='Unnamed: 0')
+image_path = drive_path + 'images/'
+
+def image_to_base64(image_full_path):
+    """Convert image file to base64"""
+    file_ = open(image_full_path, "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    return data_url
 
 def home(mode):
+    # Image resource set
+    avatar_arnold = image_to_base64(image_path+"/arnold.jpg")
+    avatar_cahya = image_to_base64(image_path+"/cahya.jpg")
+    avatar_imam = image_to_base64(image_path+"/imam.jpg")
+    avatar_rahman = image_to_base64(image_path+"/rahman.jpg")
+    avatar_ricky = image_to_base64(image_path+"/ricky.jpg")
+    avatar_teo = image_to_base64(image_path+"/teo.jpg")
+    avatar_winata = image_to_base64(image_path+"/winata.jpg")
+    
     st.title('Project Among Us')
     st.subheader('Why Among Us and Who we are')
     st.markdown("Hello Everyone! Do you know  the Among Us Game? This phenomenal game is growing fast in Google Play and App Store Platform. Some Colleagues and I who have graduated from IYKRA Data MBA Batch IV, tried to scrap, analyze, and visualize comment sections of this games. Our results can be accessed by clicking dashboard on the sidebar.")
     st.markdown("")
-    st.markdown("And Let me Introduce the team who is involved in this project:")
-    templete_html = """
+    st.markdown("And let me Introduce the team involved in this project:")
+    template_html = f"""
     <ul class="collection">
         <li class="collection-item avatar">
-            <img src="(/images/imam.jpg)" alt="images/imam.jpg" class="circle">
+            <img src="data:image/jpg;base64,{avatar_imam}"  alt="Imam Bhaskara" class="circle">
             <span class="title">Imam Bhaskara</span>
             <p>Analyst <br>
                 Linkedin: <a href="https://www.linkedin.com/in/imam-bhaskara" class="linkedin-link">Imam Bhaskara</a>
             </p>
         </li>
         <li class="collection-item avatar">
-            <img src="(/images/imam.jpg)" alt="images/imam.jpg" class="circle">
+            <img src="data:image/jpg;base64,{avatar_ricky}"  alt="Ricky Nauvaldy Ruliputra" class="circle">
             <span class="title">Ricky Nauvaldy Ruliputra</span>
             <p>Analyst <br>
                 Linkedin: <a href="https://www.linkedin.com/in/rickynauvaldy/" class="linkedin-link">Ricky Nauvaldy Ruliputra</a>
             </p>
         </li>
         <li class="collection-item avatar">
-            <img src="(/images/imam.jpg)" alt="images/imam.jpg" class="circle">
+            <img src="data:image/jpg;base64,{avatar_rahman}"  alt="Rahman Firmansyah" class="circle">
             <span class="title">Rahman Firmansyah</span>
             <p>Analyst <br>
-                Linkedin: <a href="https://www.linkedin.com/in/rahman-firmansyah-79283512b/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAB_7KR4BIiHd_IuPKvxTwNtnaMDJFlT339I" class="linkedin-link">Rahman Firmansyah</a>
+                Linkedin: <a href="https://www.linkedin.com/in/rahman-firmansyah-79283512b" class="linkedin-link">Rahman Firmansyah</a>
             </p>
         </li>
         <li class="collection-item avatar">
-            <img src="(/images/imam.jpg)" alt="images/imam.jpg" class="circle">
+            <img src="data:image/jpg;base64,{avatar_cahya}"  alt="Cahya Putera" class="circle">
             <span class="title">Cahya Putera</span>
             <p>Analyst <br>
                 Linkedin: <a href="https://www.linkedin.com/in/cahyaputera/" class="linkedin-link">Cahya Putera</a>
             </p>
         </li>
         <li class="collection-item avatar">
-            <img src="(/images/imam.jpg)" alt="images/imam.jpg" class="circle">
+            <img src="data:image/jpg;base64,{avatar_winata}"  alt="Winata Syahputera" class="circle">
             <span class="title">Winata Syahputera</span>
             <p>Analyst <br>
                 Linkedin: <a href="https://www.linkedin.com/in/winata-syahputra/" class="linkedin-link">Winata Syahputera</a>
             </p>
         </li>
         <li class="collection-item avatar">
-            <img src="(/images/imam.jpg)" alt="images/imam.jpg" class="circle">
+            <img src="data:image/jpg;base64,{avatar_teo}"  alt="Muhamad Teo Khibran" class="circle">
             <span class="title">Muhamad Teo Khibran</span>
             <p>Analyst <br>
                 Linkedin: <a href="https://www.linkedin.com/in/teokhibran/" class="linkedin-link">Muhamad Teo Khibran</a>
             </p>
         </li>
         <li class="collection-item avatar">
-            <img src="(/images/imam.jpg)" alt="images/imam.jpg" class="circle">
+            <img src="data:image/jpg;base64,{avatar_arnold}"  alt="Arnold P" class="circle">
             <span class="title">Arnold P</span>
             <p>Analyst <br>
                 Linkedin: <a href="https://www.linkedin.com/in/arnold-p/" class="linkedin-link">Arnold P</a>
@@ -71,7 +90,7 @@ def home(mode):
         </li>
     </ul>
     """
-    st.markdown(templete_html, unsafe_allow_html=True)
+    st.markdown(template_html, unsafe_allow_html=True)
 
 def dashboard_amongus(mode):
     st.title(mode)
